ae4f4fb65f37a57d9939101a4e61b3ff34cf9210
Piero <55633887+piero0920@users.noreply.github.com>
2023-03-05 04:29:28 -0500

    delete redis keys

--- redis-clean.js
+++ redis-clean.js
@@ -2,7 +2,7 @@ import Redis from './redis.js'
 
 
 async function main(){
-    const keys = await Redis.keys("BOT:#endntasu:*")
+    const keys = await Redis.keys("BOT:#endnatsu:*")
     console.log(keys)
     for(const key of keys){
         console.log(key)
