d5c2d2443c74e9c1b306f8a1a702eca6edc3010c
Piero <55633887+piero0920@users.noreply.github.com>
2023-03-07 15:39:29 -0500

    readme

---
+++ readme.md
@@ -0,0 +1,3 @@
+## status
+
+not finished yet, MYAAA
--- test.ts
+++ test.ts
@@ -5,3 +5,4 @@ console.log(2)
 //runCronjobs()
 console.log(3)
 console.log(4)
+//git remote add origin https://github.com/piero0920/pieront-bot
