ac7918c6a5ac3d6d7c9f4e95c195057a034afe15
piero <pieronakiche25@gmail.com>
2023-03-13 18:26:05 -0400

    chat settings

--- src/openai.ts
+++ src/openai.ts
@@ -13,9 +13,9 @@ export async function chatOpenAI(msg:ChatCompletionRequestMessage[], user:string
     const response = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
         messages: msg,
-        temperature: 1.5,
+        temperature: 1.2,
         top_p: 1,
-        max_tokens: 75,
+        max_tokens: 50,
         frequency_penalty: 1,
         presence_penalty: 1,
         user: user,
